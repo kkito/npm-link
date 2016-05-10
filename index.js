cdfa64f18d0822c82afc959402fd7f3a1a316611
kk.shen <kk.shen@saybot.com>
2016-05-10 15:45:07 +0800

    if cache exist do nothing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 require('shelljs/global');
+var fs = require('fs');
+
 // the cache dir of the environment ,eg: ~/.npm-link/npm/
 var base_fold = "~/.npm-link/npm/";
 mkdir('-p', base_fold);
@@ -8,21 +10,20 @@ mkdir('-p', base_fold);
 // 1, md5 package.json
 var md5str = exec("md5sum package.json  | awk '{print $1}'").stdout.trim();
 var package_fold = base_fold + "/" + md5str;
-mkdir('-p', package_fold);
-
-// if fold exist PATH/md5sum/
-
-
 
 // if exist , rm the node_modules , and link to the dir
 rm('-rf', 'node_modules');
-
-// if not , mkdir dir and link
 ln('-s' , package_fold , 'node_modules');
 
+if(!fs.existssync(package_fold)){
+	// file not exist
+	mkdir('-p', package_fold);
 
-// run npm install // or can use cnpm 
-exec('cnpm install');
+	// run npm install // or can use cnpm 
+	exec('cnpm install');
+
+}
+
 
 // bower .....
 
